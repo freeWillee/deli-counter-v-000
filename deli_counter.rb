87891cda4aa5fff318cd8588a459ce1d77049592
Will Lee <will.i.lee@outlook.com>
2018-06-07 20:01:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,9 +7,9 @@ def line(array)
   if array.empty?
     puts "The line is currently empty."
   else
-    current_line = "The line is currently: "
+    current_line = "The line is currently:"
     array.each_index do |x|
-      current_line << "#{(x+1)}. #{array[x]}"
+      current_line << " #{(x+1)}. #{array[x]}"
     end
     binding.pry
     puts current_line
