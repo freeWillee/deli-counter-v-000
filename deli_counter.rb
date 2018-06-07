6b16b8691997a89068edc59b8fc7525b9ebdb184
Will Lee <will.i.lee@outlook.com>
2018-06-07 20:14:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,9 +20,9 @@ def take_a_number(array, name)
     puts "Welcome, #{name}. You are number #{array.length} in line."
 end
 
-def now_serving
+def now_serving(array)
   if array.empty?
     puts "there is nobody waiting to be served!"
   else
-    
+    puts "Currently serving #{array.shift}."
 end
