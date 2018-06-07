1c1221d6c8bcc69e6241e92143a5f4cd0cc2c3f7
Will Lee <will.i.lee@outlook.com>
2018-06-07 19:50:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,5 +5,10 @@ katz_deli = []
 def line(array)
   if array.empty?
     puts "The line is currently empty."
+  else
+    current_line = "The line is currently: "
+    array.each_index {|x|
+      print "#{(x+1)}. #{array[x]}"
+    }
   end
 end
